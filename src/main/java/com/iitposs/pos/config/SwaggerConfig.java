e6ff3d2b13aa31035793f5e04c6f224b5186205e
Soshan Wijayarathne <soshanw123@gmail.com>
2024-10-31 13:39:02 +0530

    Added and Configured Swagger2 (A third party dependency)

--- src/main/java/com/iitposs/pos/config/SwaggerConfig.java
+++ src/main/java/com/iitposs/pos/config/SwaggerConfig.java
@@ -1,4 +1,22 @@
 package com.iitposs.pos.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+//Swagger is a third party dependency so we need to configure it
 public class SwaggerConfig {
+
+    @Bean //if not for a bean it will not put into the container
+    public Docket swaggerApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
